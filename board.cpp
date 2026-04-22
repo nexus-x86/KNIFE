58ee68ef2c8836baccb2b75c9e7c28bde20a7ba7
JustListenToTheSong <JustListenToTheSong@duck.com>
2026-04-21 20:54:23 -0400

    stuff

--- board.cpp
+++ board.cpp
@@ -1,2 +1,27 @@
 #include "board.h"
 
+void ClearBoard(Board *board) {
+    
+}
+
+void ParseFen(char *fen, Board *board);
+void BoardToFen(char *fen, Board *board);
+void PrintBoard(Board *board);
+
+void SetSpecialPieces(Board *board);
+void SetThreats(Board *board);
+
+int DoesMoveCheck(Move move, Board *board);
+int IsDraw(Board *board, int ply);
+int IsRepetition(Board *board, int ply);
+int IsMaterialDraw(Board *board);
+int IsFiftyMoveRule(Board *board);
+
+void MakeNullMove(Board *board);
+void UndoNullMove(Board *board);
+void MakeMove(Move move, Board *board);
+void MakeMoveUpdate(Move move, Board *board, int update);
+void UndoMove(Move move, Board *board);
+
+int IsPseudoLegal(Move move, Board *board);
+int IsLegal(Move move, Board *board);
--- types.h
+++ types.h
@@ -26,10 +26,47 @@ enum {
     NO_PIECE,
 },
 
-typedef struct {
+typedef uint64_t Bitboard;
+typedef uint32_t Move;
 
+typedef struct {
+    int castling;
+    int ep;
+    int fmr;
+    int nullply;
+    uint64_t zobrist;
+    uint64_t pawnZobrist;
+    Bitboard checkers;
+    Bitboard pinned;
+    Bitboard threatened;
+    Bitboard threatenedBy[6];
+    int capture;
 } BoardHistory;
 
+typedef struct {
+    int castling;
+    int epSquare;
+    int fmr;
+    int nullply;
+    uint64_t zobrist;
+    uint64_t pawnZobrist;
+    Bitboard checkers;
+    Bitboard pinned;
+    Bitboard threatened;
+    Bitboard threatenedBy[6];
+    int stm;
+    int xstm;
+    int histPly;
+    int moveNo;
+    int phase;
+    uint64_t piecesCounts;
+    int squares[64];
+    Bitboard occupancies[3];
+    Bitboard pieces[12];
+    int cr[4];
+    int castlingRights[64];
+    BoardHistory history[300]; // random constant
+} Board;
 
 
 #endif
