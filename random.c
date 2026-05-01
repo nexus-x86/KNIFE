7519185ff178b573151e9c538fa89827193b782e
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-05-01 14:38:50 -0400

    random surely

---
+++ random.c
@@ -0,0 +1,31 @@
+#include "random.h"
+
+#include <inttypes.h>
+
+// pro tip: never code your own random number generator.
+//          always copy someone else
+
+uint64_t keys[2];
+
+inline uint64_t rotate(uint64_t v, uint8_t s) {
+  return (v >> s) | (v << (64 - s));
+}
+
+inline uint64_t RandomUInt64() {
+  uint64_t tmp = keys[0];
+  keys[0] += rotate(keys[1] ^ 0xc5462216u ^ ((uint64_t) 0xcf14f4ebu << 32), 1);
+  return keys[1] += rotate(tmp ^ 0x75ecfc58u ^ ((uint64_t) 0x9576080cu << 32), 9);
+}
+
+void SeedRandom(uint64_t seed) {
+  keys[0] = seed;
+  keys[1] = seed;
+
+  for (int i = 0; i < 64; i++) {
+    RandomUInt64();
+  }
+}
+
+uint64_t RandomMagic() {
+  return RandomUInt64() & RandomUInt64() & RandomUInt64();
+}
