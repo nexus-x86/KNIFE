d1ac618b295aee41e5524749fb47012527ec60cb
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-05-04 11:54:48 -0400

    zobrist

--- zobrist.c
+++ zobrist.c
@@ -6,6 +6,12 @@ uint64_t ZOBRIST_EP_KEYS[64];
 uint64_t ZOBRIST_CASTLE_KEYS[16];
 uint64_t ZOBRIST_SIDE_KEY;
 
+static inline int PopLSB(Bitboard *bb) {
+  int sq = LSB(*bb);
+  *bb &= *bb - 1;
+  return sq;
+}
+
 void InitZobristKeys() {
   for (int i = 0; i < 12; i++) {
     for (int j = 0; j < 64; j++) {
@@ -30,6 +36,36 @@ uint64_t Zobrist(Board* board) {
   for (int piece = WHITE_PAWN; piece <= BLACK_KING; piece++) {
     Bitboard pcs = board->pieces[piece];
     while (pcs) {
-      hash ^= ZOBRIST_PIECES[piece]
+      hash ^= ZOBRIST_PIECES[piece][PopLSB(&pcs)];
+    }
   }
+
+  if (board->epSquare) {
+    hash ^= ZOBRIST_EP_KEYS[board->epSquare];
+  }
+
+  hash ^= ZOBRIST_CASTLE_KEYS[board->castling];
+
+  if (board->stm) {
+    hash ^= ZOBRIST_SIDE_KEY;
+  }
+
+  return hash;
+}
+
+uint64_t PawnZobrist(Board *board) {
+  uint64_t hash = 0ULL;
+
+  for (int piece = WHITE_PAWN; piece <= BLACK_PAWN; piece++) {
+    Bitboard pcs = board->pieces[piece];
+    while (pcs) {
+      hash ^= ZOBRIST_PIECES[piece][PopLSB(&pcs)];
+    }
+  }
+
+  if (board->stm) {
+    hash ^= ZOBRIST_SIDE_KEY;
+  }
+
+  return hash;
 }
