0e2d80e2f80e74f415aeceefcb9b5d8a37681d72
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-05-03 23:45:18 -0400

    zobrist progress

--- zobrist.c
+++ zobrist.c
@@ -8,4 +8,28 @@ uint64_t ZOBRIST_SIDE_KEY;
 
 void InitZobristKeys() {
   for (int i = 0; i < 12; i++) {
-    for (int j = 0; j <
+    for (int j = 0; j < 64; j++) {
+       ZOBRIST_PIECES[i][j] = RandomUInt64();
+    }
+  }
+
+  for (int i = 0; i < 64; i++) {
+    ZOBRIST_EP_KEYS[i] = RandomUInt64();
+  }
+
+  for (int i = 0; i < 16; i++) {
+    ZOBRIST_CASTLE_KEYS[i] = RandomUInt64();
+  }
+
+  ZOBRIST_SIDE_KEY = RandomUInt64();
+}
+
+uint64_t Zobrist(Board* board) {
+  uint64_t hash = 0ULL;
+
+  for (int piece = WHITE_PAWN; piece <= BLACK_KING; piece++) {
+    Bitboard pcs = board->pieces[piece];
+    while (pcs) {
+      hash ^= ZOBRIST_PIECES[piece]
+  }
+}
