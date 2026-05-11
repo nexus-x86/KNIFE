38c4724f030be74a7c688889f7a697f7e0748be0
JustListenToTheSong <JustListenToTheSong@duck.com>
2026-05-10 23:31:15 -0400

    a

--- random.h
+++ random.h
@@ -1,11 +1,14 @@
-#ifndef RANDOM_H
-#define RANDOM_H
+#pragma once
 
+#include <cstdint>
+
+class PRNG {
+    public:
+    PRNG() : keys()
+}
 #include <inttypes.h>
 
 uint64_t rotate(uint64_t v, uint8_t s);
 uint64_t RandomUInt64();
 void SeedRandom(uint64_t seed);
 uint64_t RandomMagic();
-
-#endif
