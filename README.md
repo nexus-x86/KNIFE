fa230880dfb013af126fc05c7f24d5a003894664
JustListenToTheSong <JustListenToTheSong@duck.com>
2026-05-10 22:10:21 -0400

    guhh

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ A chess engine with the core design philosophy being precise aggression.
 
 ## Inspirations
 - [berserk](https://github.com/jhonnold/berserk)
+- [Altair](https://github.com/Alex2262/AltairChessEngine)
 - [Stockfish](https://github.com/official-stockfish/Stockfish)
 - [chessinator](https://github.com/dogeystamp/chess_inator)
---
+++ uci.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <thread>
+#include <vector>
+#include "position.h"
+#include "search.h"
+
+class UCI {
+    public:
+        UCI() = default;
+        ~UCI() = default;
+
+        std::string msg{};
+        std::vector<std::string> tokens{};
+
+        std::unique_ptr<Engine> engine = std::make_unique<Engine>();
+        Move last_move = NO_MOVE;
+
+        std::vector<std::thread> search_threads{};
+
+        void initialize_uci() const;
+        void parse_position();
+        void parse_go();
+        void uci_loop();
+}
